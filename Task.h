7c19629221a2310189b6725bde48b25b34e8feed
lotu <gao527950554@foxmail.com>
2019-03-19 20:38:39 +0800

    coding complete, waiting for test.

---
+++ Task.h
@@ -0,0 +1,35 @@
+#ifndef THREAD_POOL_TASK_H
+#define THREAD_POOL_TASK_H
+
+#include<functional>
+#include<ctime>
+
+using Task = std::function<void ()>;
+namespace ThreadPool
+{
+    struct Timertask{
+
+    bool _flag;
+    Task _task;
+    int64_t _timeval;
+    int64_t _exec_time;
+
+    bool operator<(const struct Timertask& other) const{
+        return _exec_time > other._exec_time;
+    }
+
+    Timertask(const Task& task,int64_t timeval,int64_t exec_time,bool flag = false)
+        :_flag(flag),_task(task),_timeval(timeval),_exec_time(exec_time)
+        {}
+
+    Timertask(const Task& task,int64_t timeval,bool flag = false)
+        :Timertask(task,timeval,timeval+time(NULL),flag)
+        {}
+    
+    };
+} // ThreadPool
+
+
+
+
+#endif
---
+++ ThreadPool.h
@@ -0,0 +1,59 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include "task.h"
+
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+#include <queue>//priority_queue
+
+
+namespace ThreadPool
+{
+    class ThreadPool
+    {
+    private:
+        std::atomic<uint64_t> _pending_num;
+        std::atomic<uint64_t> _running_num;
+        uint64_t _task_count;
+
+        bool _stop;
+        int _thread_num;
+        std::vector<std::thread> _ths;
+
+        std::mutex _mtx;
+        std::condition_variable _cond;
+        std::deque<Task> _queue;
+        std::priority_queue<Timertask> _timer_queue;
+
+    public:
+        ThreadPool(int thread_num);
+        ~ThreadPool();
+
+        bool Start();
+        bool Stop(bool graceful);
+
+        void AddTask(const Task& task);
+        void AddPriorityTask(const Task& task);
+        void AddDelayTask(int timeval,const Task& task);
+        void AddTimerTask(int timeval,const Task& task);
+
+        bool isEmpty(){return _running_num > 0;}
+        bool CancelTask();
+
+        static void *ThreadFunc(void* arg)
+        {
+            reinterpret_cast<ThreadPool*>(arg)->process();
+            return 0;
+        }
+        void process();
+    };
+
+} // ThreadPool
+
+
+#endif
